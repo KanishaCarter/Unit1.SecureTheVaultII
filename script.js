f49afbcf648b597b7360d32903fa44a99f768a84
KanishaCarter <kanishacarter90@outlook.com>
2025-05-30 16:12:34 -0500

    completed secure the vault workshop with no errors.

---
+++ script.js
@@ -0,0 +1,10 @@
+// Code is 10 - 40 - 39
+
+// code1 uses addition to get the sum (10)
+const code1 = 5 + 5;
+// code2 uses multiplication to get the product (40)
+const code2 = 10 * 4;
+// code3 uses multiplaction and addition to calculate the third code
+const code3 = 10 * 3 + 9;
+// combination code is logged to the console
+console.log(`${code1} - ${code2} - ${code3}`);
